52fc06b953c30be6e088c5b88cde28c88854b639
pentonvillefandango <marcsheldon1@gmail.com>
2026-01-11 10:23:19 +0000

    added player, player LR UD Movement

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ VERSION = pygame.version.ver
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -12,13 +13,19 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player1.draw(screen)
+        player1.update(dt)
         pygame.display.flip()
+        dt = clock.tick(60)
 
 
 if __name__ == "__main__":
